f5983c5ee44b046c9c2ac5c698da9e73502409d8
Alex Burner <alex.burner@gmail.com>
2014-04-26 12:34:32 -0700

    no update console

--- newpublic/js/socketHandler.js
+++ newpublic/js/socketHandler.js
@@ -73,8 +73,6 @@ socket.on(Packet.UPDATE_ENTITY, function (data) {
 			}
 		} else if (data.entity.type === EntityType.PROJECTILE) {
 			entitiesByID[data.entity.id].update(data.entity);
-
-			console.log('updated');
 			if(data.entity.userName === currentUser.name) {
 				onFrame((new Date()).getTime());
 			}
